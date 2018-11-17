ac4c045c6d2433753f45fc4ec4343f6ea503febf
Melinda Lu <lumel@berkeleyprep.org>
2018-11-17 17:13:45 -0500

    test

--- DeckTester.java
+++ DeckTester.java
@@ -9,5 +9,15 @@ public class DeckTester {
 	 */
 	public static void main(String[] args) {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
+	String ranks2[] = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jacks", "Queens", "Kings"};
+	String suits2[] = {"Spades", "Clubs", "Hearts", "Diamonds"};
+	int pointValue2[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,};
+
+	Deck e = new Deck(ranks2, suits2, pointValue2);
+
+	for (int i = 0; i < ranks2.length; i++) {
+		System.out.println(e.deal());
+	}
+	System.out.println(e.isEmpty());
 	}
 }
